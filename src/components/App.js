43a5c0721cbd9c829f9238899b891b35898ab213
mahziyar110 <mahziyar110@gmail.com>
2021-09-01 13:05:33 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ class App extends Component {
   render() {
     return (
       <div id="main">
-        <ol key={relativeList}>
-          <li key={relativeListItem1}>Relative 1</li>
-          <li key={relativeListItem2}>Relative 2</li>
-          <li key={relativeListItem3}>Relative 3</li>
-          <li key={relativeListItem4}>Relative 4</li>
+        <ol key="relativeList">
+          <li key="relativeListItem1">Relative 1</li>
+          <li key="relativeListItem2">Relative 2</li>
+          <li key="relativeListItem3">Relative 3</li>
+          <li key="relativeListItem4">Relative 4</li>
         </ol>
       </div>
     );
